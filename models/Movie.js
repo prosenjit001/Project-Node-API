6d92fbe151e79bcd3430eb0c42a722e055651e09
prosenjit001 <mitra.prosenjit2008@gmail.com>
2016-10-25 10:14:01 +0400

    Add files via upload

---
+++ models/Movie.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+//Create MovieSchema
+var MovieSchema = new mongoose.Schema({
+	title:{type:String,required:true},
+	url:{type:String,required:true}
+});
+
+//Export the model schema
+module.exports = MovieSchema;
+
